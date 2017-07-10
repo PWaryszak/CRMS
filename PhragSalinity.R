810520c5e9fdf1791157f315843464c105458aba
pwaryszak <dorpatczyk@yahoo.com>
2017-07-10 13:35:40 -0500

    Salinity Change and Cover Change over 10 year period computed in plots where Phragmites was recorded + "Change" figures.

--- PhragSalinity.R
+++ PhragSalinity.R
@@ -1,94 +1,123 @@
-#Filter out plots with Phrag:
+#LOAD VEG DATA============
 library(tidyverse)
 
 veg <- read.csv("CRMS_Marsh_Veg.csv")#From cleaned the CRMS_Marsh_Vegetation.csv to suit R.
-str(veg)#293726 obs. of  24 variables:
+str(veg)#133612 obs. of  24 variables:
 levels(veg$Community)#"Brackish" "Freshwater" "Intermediate" "Saline"  "Swamp"
-length(levels(veg$SpecCode))#747, AaaProblem and  Wete are gone as these were not species (bare ground, Wate (open water))see comments in "CRMSDataCleaning" R file
+length(levels(veg$SpecCode))#430, see comments in "CRMSDataCleaning.R" file to learn more
 veg$Community<-factor(veg$Community, levels = c( "Swamp","Freshwater","Intermediate","Brackish","Saline"))#re-arranging levels ac to salinity levels
 
-#Where is Phragmites? Phrag Cover Plot============
+#Filter out plots with Phrag only:
 Phragmites<-filter(veg, SpecCode == "Phraaust") #subsetting only data we are most interested in code 1&2 = tavy
-str(Phragmites)#3031 obs. of  28 variables
+str(Phragmites)#799 obs. of  24 variables:
 View(Phragmites)
+
 PhragPresence<-as.data.frame(table(Phragmites$StationFront))
+PhragPresence#Phrag occurences by Station (site)
 PhragStations<-PhragPresence[PhragPresence$Freq > 0 , ]#selecting Station with more than 0 cover of Phrag
-length(levels(droplevels(PhragStations$StationFront)))#89 Stations recorded Phrag in their plots,
-colnames(PhragStations)[1] <- "StationFront"
 Abundant <- PhragStations[order(- PhragStations$Freq),] #sorting in descending mannert to have a look 
-#what station are most abundant in Phragmites
-Abundant
-head(PhragStations)
+Abundant#what stations are the most abundant in Phragmites?
+
+colnames(PhragStations)[1] <- "StationFront" #renaming Var1 column to what it is = StationFront.
+length(levels(droplevels(PhragStations$StationFront)))#52 Stations recorded Phrag in their plots that have consiste salinity data
 levels(droplevels(Phragmites$StationFront))
 
+#Phrag to Salinity Correlation==========
 #Let us subset the PhragStations from env data:
 env<-read.csv("CRMS_Soil.csv")
 envPhrag<- env[ which ( env$StationFront %in% PhragStations$StationFront) , ]
-str(envPhrag)#1485 obs. of  19 variables:
+str(envPhrag)#23458 obs. of  12 variables:
 table(envPhrag$year)
-#2006 2007 2008 2009 #only these years were measured and had Phrag in it.
-#378  600  417   90
+#2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 
+#389 1600 2499 2876 2936 2590 3126 2645 1656 1249 1615  277
+envPhrag2007to2016<-envPhrag[! envPhrag$year=="2006"& ! envPhrag$year=="2017",] #removing low n years
+table(envPhrag2007to2016$year)
+#2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 
+#1600 2499 2876 2936 2590 3126 2645 1656 1249 1615 
+
+
 #Compute mean salinity per StationFront. There were ~10 StationsID-s per StationFront
-envPhragSalinity<-summarise(group_by(envPhrag,StationFront,year), MeanSalinity=mean(SoilSalinity.ppt),
-                SDSalinity=sd(SoilSalinity.ppt),CVSalinity= SDSalinity/MeanSalinity)
-envPhragSalinity #tible 85x5 = 85 stations only were measured that had Phrag in them
+envPhragSalinity<-summarise(group_by(envPhrag2007to2016,StationFront,year), MeanSalinity=mean(SoilPorewaterSalinity.ppt),
+                SDSalinity=sd(SoilPorewaterSalinity.ppt),CVSalinity= SDSalinity/MeanSalinity)
+#View(envPhragSalinity) # data frame [505 x 5]
+envPhragSalinity$StationFront.Year<-interaction(envPhragSalinity$StationFront,envPhragSalinity$year)
 
-#For year 06 & 07 these saliniites do not sync.
-#let us see what plots were measured consistently across years:
-PlotsMeasured<-summarise(group_by(env,StationID,year), number=length(StationID)) #summarize
-PlotsMeasured2<-filter(PlotsMeasured, year==2006|year==2007|year==2008| year==2009)#select 
-PlotsMeasured2
-PlotsMeasured3<-spread(PlotsMeasured2, key = year, value = number,fill=0)
-M<-PlotsMeasured3[ , 2:5]
-M[M>0] <- 1 #Change values to 0 1 
-M$suming<-rowSums(M)
-range(M$suming)#1 1 #if none of the rows is bigger than one it means 
-#none of the years overlap in measuring salinity
+#Merge Phrag data with salinity data:
+#Produce new unique column "StationFront.Year" to be merged by:
+Phragmites$StationFront.Year<-interaction(Phragmites$StationFront, Phragmites$year)
+Phrag<- Phragmites[ , c("StationFront.Year","StationID", "Cover","SpecCode")]
 
-#EXAMPLE BELOW for no overlap in measurments between 2006 & 2007==============
-#Change in Phrag cover (06-07) + change in salinity over these years = lack of data to do this:
-Phragmites<-filter(veg, SpecCode == "Phraaust") #subsetting only data we are most interested in code 1&2 = tavy
-str(Phragmites)#3031 obs. of  28 variables
+PhragCoverSalinity <- dplyr::full_join(Phrag,envPhragSalinity, by = "StationFront.Year" )
+dim(PhragCoverSalinity)# 1019   obse = initial Phrag dimensions. YAY!
+head(PhragCoverSalinity)
 
-#YEAR 2006:
-Phrag06<- Phragmites[Phragmites$year=="2006", c("StationID", "StationFront", "Cover")]
-str(Phrag06)#83 obs. of  3 variables:
-length(levels(droplevels(Phrag06$StationID)))#83 plots
-length(levels(droplevels(Phrag06$StationFront)))#21 Stations with Phragmites
+#GGPLOT with not outliers:
+p <- ggplot(PhragCoverSalinity, aes( MeanSalinity,Cover))
+p2<-p + geom_point(aes(color=MeanSalinity)) +theme_classic()
+p2 +ggtitle("Mean Salinity and Phragmites Cover over 2007-2016 period")
 
-#Subset Stations measured in 2006:
-plots06<-envPhragSalinity[envPhragSalinity$year=="2006",]
-plots06#21x5
+#Two outliers + NAs to be removed:
+PhragCoverSalinity2<-PhragCoverSalinity[- which.max(PhragCoverSalinity$MeanSalinity),]
+PhragCoverSalinity3<-PhragCoverSalinity2[- which.max(PhragCoverSalinity2$MeanSalinity),]
+PhragCoverSalinity4<-PhragCoverSalinity3[ - is.na(PhragCoverSalinity2$MeanSalinity),]
+PhragCoverSalinity4$year<-as.factor(PhragCoverSalinity4$year)
+dim(PhragCoverSalinity4)#796   9
+PhragCoverSalinity5<-PhragCoverSalinity4[!is.na(PhragCoverSalinity4$StationFront),]
+dim(PhragCoverSalinity5) #780 9
 
-envPhrag06<-envPhragSalinity[ which(envPhragSalinity$StationFront %in% plots06$StationFront) , c("StationFront","MeanSalinity","CVSalinity", "year") ]
-length(levels(droplevels(envPhrag06$StationFront)))#21 Stations with Phragmites
-str(envPhrag06)#378 obs. of  3 variables:
+#GGPLOT with not outliers:
+pp <- ggplot(PhragCoverSalinity5, aes( MeanSalinity,Cover))
+pp2<-pp + geom_point(aes(color=MeanSalinity)) +theme_classic()
+pp2 +ggtitle("Mean Salinity and Phragmites Cover over 2007-2016 period")
 
-#Merge Phrag06 stations with envPhrag06 Stations
-Joined06 <- dplyr::left_join(Phrag06,envPhrag06, by = "StationFront" )
-Joined06
-Joined06<-Joined06[ ! is.na(Joined06$MeanSalinity),] #removing one NA row
-Joined06
-#YEAR 2007:
-Phrag07<- Phragmites[Phragmites$year=="2007", c("StationID", "StationFront", "Cover")]
-str(Phrag07)#218 obs. of  3 variables:
-length(levels(droplevels(Phrag07$StationID)))#218 plots
-length(levels(droplevels(Phrag07$StationFront)))#47 Stations with Phragmites
 
-#Subset Stations measured in 2007:
-plots07<-envPhragSalinity[envPhragSalinity$year=="2007",]
-plots07#35x5
-envPhrag07<-envPhragSalinity[ which(envPhragSalinity$StationFront %in% plots07$StationFront) , c("StationFront","MeanSalinity","CVSalinity", "year") ]
-length(levels(droplevels(envPhrag07$StationFront)))#35 Stations with Phragmites
+#Phrag Cover Change to Salinity Change Correlation=============
+dim(PhragCoverSalinity5) #780 9 - see above how we got here.
+#spread Cover over years 
+CoverChange<- PhragCoverSalinity5[ , c("StationID", "Cover", "StationFront", "year")]
+CoverChangeWide <- spread(CoverChange, key = year, value = Cover)
+dim(CoverChangeWide)#191  12
+CoverChangeWide$CoverChange1<- - CoverChangeWide$`2007` + CoverChangeWide$`2008`
+CoverChangeWide$CoverChange2<- - CoverChangeWide$`2008` + CoverChangeWide$`2009`
+CoverChangeWide$CoverChange3<- - CoverChangeWide$`2009` + CoverChangeWide$`2010`
+CoverChangeWide$CoverChange4<- - CoverChangeWide$`2010` + CoverChangeWide$`2011`
+CoverChangeWide$CoverChange5<- - CoverChangeWide$`2011` + CoverChangeWide$`2012`
+CoverChangeWide$CoverChange6<- - CoverChangeWide$`2012` + CoverChangeWide$`2013`
+CoverChangeWide$CoverChange7<- - CoverChangeWide$`2013` + CoverChangeWide$`2014`
+CoverChangeWide$CoverChange8<- - CoverChangeWide$`2014` + CoverChangeWide$`2015`
+CoverChangeWide$CoverChange9<- - CoverChangeWide$`2015` + CoverChangeWide$`2016`
 
-#Merge Phrag07 stations with envPhrag07 Stations
-Joined07 <- dplyr::left_join(Phrag07,envPhrag07, by = "StationFront" )
-names(Joined07)<-c("StationID","StationFront", "Cover07" ,"MeanSalinity07", "CVSalinity07", "year07")
-Joined07<-Joined07[,-2]# We do not need this column for merging qirh Joined06
-Joined07
-Joined07<-Joined07[ ! is.na(Joined07$MeanSalinity07),] #removing one NA row
-Joined07
+#Spread Salinity over years:
+SalinityChange<- PhragCoverSalinity5[ , c(  "StationID","MeanSalinity", "StationFront" ,"year")]
+#SalinityChange$rows<- 1:nrow(SalinityChange)
+SalinityChangeWide <- spread(SalinityChange, key = year, value = MeanSalinity)
+dim(SalinityChangeWide)#191  12
+SalinityChangeWide
+SalinityChangeWide$SalinityChange1<- - SalinityChangeWide$`2007`+ SalinityChangeWide$`2008`
+SalinityChangeWide$SalinityChange2<- - SalinityChangeWide$`2008`+ SalinityChangeWide$`2009`
+SalinityChangeWide$SalinityChange3<- - SalinityChangeWide$`2009`+ SalinityChangeWide$`2010`
+SalinityChangeWide$SalinityChange4<- - SalinityChangeWide$`2010`+ SalinityChangeWide$`2011`
+SalinityChangeWide$SalinityChange5<- - SalinityChangeWide$`2011`+ SalinityChangeWide$`2012`
+SalinityChangeWide$SalinityChange6<- - SalinityChangeWide$`2012`+ SalinityChangeWide$`2013`
+SalinityChangeWide$SalinityChange7<- - SalinityChangeWide$`2013`+ SalinityChangeWide$`2014`
+SalinityChangeWide$SalinityChange8<- - SalinityChangeWide$`2014`+ SalinityChangeWide$`2015`
+SalinityChangeWide$SalinityChange9<- - SalinityChangeWide$`2015`+ SalinityChangeWide$`2016`
 
-#Compare Joined06 with Joined07:
-Year06_07<- left_join(Joined06,Joined07, by = "StationID")
-Year06_07
+PhragCoverSalinityChange<- cbind (CoverChangeWide, SalinityChangeWide)
+head(PhragCoverSalinityChange)
+
+#GGPLOT of change of cover vs salinity:
+Change1 <- ggplot(data = PhragCoverSalinityChange, aes( CoverChange1,SalinityChange1))+ geom_point()+geom_smooth() +theme_classic()+ggtitle("Over 2007-2008 period")+geom_hline(yintercept=0, linetype = 2)
+Change2 <- ggplot(data = PhragCoverSalinityChange, aes( CoverChange2,SalinityChange2))+ geom_point()+geom_smooth() +theme_classic()+ggtitle("Over 2008-2009 period")+geom_hline(yintercept=0, linetype = 2)
+Change3 <- ggplot(data = PhragCoverSalinityChange, aes( CoverChange3,SalinityChange3))+ geom_point()+geom_smooth() +theme_classic()+ggtitle("Over 2009-2010 period")+geom_hline(yintercept=0, linetype = 2)
+Change4 <- ggplot(data = PhragCoverSalinityChange, aes( CoverChange4,SalinityChange4))+ geom_point()+geom_smooth() +theme_classic()+ggtitle("Over 2010-2011 period")+geom_hline(yintercept=0, linetype = 2)
+Change5 <- ggplot(data = PhragCoverSalinityChange, aes( CoverChange5,SalinityChange5))+ geom_point()+geom_smooth() +theme_classic()+ggtitle("Over 2011-2012 period")+geom_hline(yintercept=0, linetype = 2)
+Change6 <- ggplot(data = PhragCoverSalinityChange, aes( CoverChange6,SalinityChange6))+ geom_point()+geom_smooth() +theme_classic()+ggtitle("Over 2012-2013 period")+geom_hline(yintercept=0, linetype = 2)
+Change7 <- ggplot(data = PhragCoverSalinityChange, aes( CoverChange7,SalinityChange7))+ geom_point()+geom_smooth() +theme_classic()+ggtitle("Over 2013-2014 period")+geom_hline(yintercept=0, linetype = 2)
+Change8 <- ggplot(data = PhragCoverSalinityChange, aes( CoverChange8,SalinityChange8))+ geom_point()+geom_smooth() +theme_classic()+ggtitle("Over 2014-2015 period")+geom_hline(yintercept=0, linetype = 2)
+Change9 <- ggplot(data = PhragCoverSalinityChange, aes( CoverChange9,SalinityChange9))+ geom_point()+geom_smooth() +theme_classic()+ggtitle("Over 2015-2016 period")+geom_hline(yintercept=0, linetype = 2)
+
+require(gridExtra)
+grid.arrange(Change1,Change2,Change3, ncol=3)
+grid.arrange(Change4,Change5,Change6, ncol=3)
+grid.arrange(Change7,Change8,Change9, ncol=3)
