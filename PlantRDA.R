338fbcd2aa755e82232913fdd3fcec319ff9a422
pwaryszak <dorpatczyk@yahoo.com>
2017-07-20 21:41:53 -0500

    RDA analysis was run (~2h) for freshwater communites + fancy ggplotting of RDA1 & RDA2.

--- PlantRDA.R
+++ PlantRDA.R
@@ -1,4 +1,4 @@
-#Produce veg data for RDA ANALYSIS ==========
+#Produce veg data and matching env data for RDA ANALYSIS ==========
 # Load packages
 library(vegan)
 library(tidyverse)
@@ -17,7 +17,9 @@ length(levels(droplevels(samples2007$StationID)))#2558 = number of good plots in
 
 DF2007to2016<- veg[ which (veg$StationID  %in%  samples2007$StationID), c("year", "StationID", "StationFront","StationBack", "SpecCode", "Cover", "Community", "CoverTotal")]
 str(DF2007to2016)#118570 obs. of  8 variables:
-table(DF2007to2016$year)# a quite balanced sampling!!!!
+table(DF2007to2016$year)# a well balanced sampling!!!!
+#2007  2008  2009  2010  2011  2012  2013  2014  2015  2016 
+#11757 11225 11624 11692 11527 11573 11936 12473 12504 12259 
 
 #Reshape to wide format to compute plant composition indices:
 DF2007to2016$Cover <- ifelse(DF2007to2016$Cover ==0,0,1) #turning cover to presence/absence data
@@ -26,7 +28,7 @@ v<-DF2007to2016[,c("StationID","StationFront","Community","SpecCode","Cover","Co
 v.wide<-spread(v,key = SpecCode, value = Cover, fill = 0)#species indices can be computed in a wide format only= each species has its own column.
 #write.csv(v.wide, file = "VegConsistentPlotsData.csv", row.names = FALSE)
 
-#RDA Analysis, with For Loop ===========
+#Veg Dissimilarity Analysis across years, with For Loop ===========
 #TRAIL:
 #Subset one plot for trial RDA run for Christina:
 CRMS0002_V54<- v.wide[v.wide$StationID=="CRMS0002_V54",]
@@ -36,11 +38,9 @@ CRMS0002_V54[, 1:5] #years not in asc order
 trail<-CRMS0002_V54[ order(CRMS0002_V54$year),]
 trail[,1:5]
 dim(trail)#10 409
-
 #Compute Disimilarity distances:
 DisTrail <- vegdist(trail[6:409], distance = "bray", binary = TRUE, upper=TRUE, diag=FALSE)
 class(DisTrail)
-
 #Convert Dis to matrix"
 m<-as.matrix(DisTrail)
 colnames(m) <- c( "Year2007", "Year2008", "Year2009","Year2010","Year2011","Year2012","Year2013","Year2014","Year2015","Year2016")
@@ -52,6 +52,7 @@ m2<-as.data.frame(m)
 #This will givethe average dissimilarity of each year to all other years:
 m3<- colMeans(m2, na.rm = TRUE)
 m3
+
 
 #CREATE FOR LOOP to repeat these actions across all plots:
 
@@ -91,13 +92,13 @@ Output2$StationFront<- substr(Output2$StationID, 1,8)
 str(Output2)#25590 obs. of  4 variables:
 #write.csv(Output2, file="DissimilarityOutput.csv")
 
-#Q: How dissimilar plots are per community types:==========
+#Dissimilarity Analysis per community types:==========
 #Get veg data and define StationsFront (Site) prevelent community:
 str(veg)#133612 obs. of  24 variables
 levels(veg$Community)#"Brackish" "Freshwater" "Intermediate" "Saline"
 veg$Community<-factor(veg$Community, levels = c( "Freshwater","Intermediate","Brackish","Saline"))#re-arranging levels ac to salinity levels
 
-#Define the community-type in each StationFront based on  n of Comms:
+#Define the community-type in each StationFront based on  n of Commuity-typ Counts per station:
 StationComm<- group_by(veg,StationFront,Community ) %>% count(Count=StationFront)
 StationComm#It gives us count of communities per StationFront (740)
 SCwide<- spread(StationComm, key = Community, value = n, fill = 0)#make it wide
@@ -106,6 +107,8 @@ SCwide
 StationCommDefined<-SCwide[, c(1,7)]
 colnames(StationCommDefined)[2] <- "Community" #Renaming WhichMAx back to Community
 StationCommDefined #320 stationFronts
+
+#GGPLOT of variability over time=========
 
 #Join Dissimilarity Output2 with Community-type variables:
 DisOutput<-read.csv("DissimilarityOutput.csv")#running For loop takes times so I saved output in this file
@@ -117,91 +120,134 @@ head(DissCommunity, n= 1)
 length(levels(DissCommunity$StationID))#2558 YAY! as many as before for loop.
 unique(DissCommunity$year2rest)#10 levels
 
-#GGPLOT of variability over time=========
+#GGPLOT:
 overtime<-summarize(group_by(DissCommunity,year2rest,Community), MeanDiss = mean(Average_Dissimilarity))
 overtime$Community<-factor(overtime$Community, levels = c("Freshwater","Intermediate","Brackish","Saline"))#re-arranging levels ac to salinity levels
-
 VariabilityPlot <- ggplot(overtime, aes(y = Community, x = MeanDiss,color=year2rest,shape =year2rest))+ geom_point(position = "jitter", size=4)
 VariabilityPlot + scale_shape_manual(values = c(15,15,16,16,18,18,24,24,13,13)) +theme_classic() +ggtitle("Mean community variability across time")
 
 
+#Run RDA by Community==============
+library(vegan);library(ggplot2);library(tidyr);library(dplyr);require(grid)
+###The Bray-Curtis dissimilarity is bounded between 0 and 1, 
+###where 0 means the two sites have the same composition (that is they share all the species), 
+###and 1 means the two sites do not share any species
+#LOAD VEG DATA:
+vegdata<<- read.csv(url("https://sites.google.com/site/phragmitesproject/file-cabinet/VegDataAll.csv?attredirects=0"))
+dim(vegdata) #to see how many rows and columns are there = 25580 rows by  404 cols
+#scanning for zero columns (singletons)
+occur.col<-apply(vegdata,2,sum)#function that compute sums per colums
+zerosCOL<-vegdata[occur.col <= 0 ,]#less or equal zero
+nrow(zerosCOL)#number of zero rows = 0 YAY!
+#scanning for zero rows
+occur.row<-apply(vegdata,1,sum)#function that compute sums per colums
+zerosROW<-vegdata[occur.row <= 0 ,]#less or equal zero
+nrow(zerosROW)#number of zero rows = 1 = needs removing!
+cutROWS<-rownames(vegdata[occur.row <= 0 ,])
+cutROWS#"5374" this row needs to be cut out of both veg and env data
+
+#LOAD ENV DATA:
+envdata<- read.csv(url("https://sites.google.com/site/phragmitesproject/file-cabinet/EnvDataAll.csv?attredirects=0"))
+dim(envdata)#25580    11
+vegenv<-cbind(vegdata,envdata)# Binding veg matrix and env data together
+dim(vegenv)# 25580   415
+vegenv2<- vegenv[-5374,]
+dim(vegenv2)#25579   415
+
+#SUBSET COMMUNITIES:
+#FRESHWATER:
+freshOnly <- vegenv2[ vegenv2$Community=="Freshwater",]
+head(freshOnly)
+sum(is.na(freshOnly$MeanSalinity)) #51 rows with na need removing
+sum(is.na(freshOnly$PhragCover)) #0 rows with na need removing
+dim(freshOnly)#5442  415
+OurNArows<- is.na(freshOnly$MeanSalinity) #51 rows with na need removing
+5442 - 51 # = 5391
+freshOnly1 <- freshOnly[ ! OurNArows,]
+dim(freshOnly1 )#5391  415
+freshVeg<-freshOnly1[,1:404] #Freshwater  veg community data only
+head(freshVeg)
+freshEnv<-freshOnly1 [ , -c(1:404)] #Freshwater env data only
+head(freshEnv)
+
+##BRAY Vegetation distance matrix construction
+#FRESHWATER:
+BRAYfresh <- vegdist(freshVeg, distance = "bray")
+str(BRAYfresh)#Class 'dist'  atomic [1:14528745]
+
+df.response1 <- decostand( BRAYfresh, method = 'hellinger' )
+str(df.response1)# num [1:5391, 1:5391]
+
+fresh_rda <- rda(df.response1 ~ MeanSalinity + PhragCover, freshEnv)
+str(fresh_rda)
+
+# PLOTS (quick ones to see)
+plot(fresh_rda, display=c("cn", "sp", "lc"), main="Lousiana Freshwhater Communities")
+
+plot(fresh_rda, display=c("lc","cn"), main="Lousiana Freshwhater Communities")
+
+# GRAPHING RDA with ggplot2
+# Use the "scores" function, then use the elements of it, casting them to data frames, e.g.:
+df.sites <- as.data.frame( scores(fresh_rda)$sites )
+
+# The environment variables are in another element, e.g.:
+# $CCA$biplot gives the biplot coords for the env variables 
+df.env <- as.data.frame( fresh_rda$CCA$biplot[, 1:2] )
+df.env
+df.env$var <- rownames( fresh_rda$CCA$biplot )
+
+df.env$xOrg <- 0 #for plotting arrows
+df.env$yOrg <- 0 #for plotting arrows
+
+summary(fresh_rda)
+#Accumulated constrained eigenvalue, Importance of components:
+#                        RDA1      RDA2
+#Eigenvalue            1.881e-05 2.088e-06
+#Proportion Explained  9.001e-01 9.991e-02
+#Cumulative Proportion 9.001e-01 1.000e+00
+
+#             RDA1     RDA2 PC1 PC2 PC3 PC4
+#MeanSalinity 0.1317  0.99128   0   0   0   0
+#PhragCover   0.9979 -0.06419   0   0   0   0
+
+# R2 of the variance explained
+R2 <- RsquareAdj(fresh_rda)$adj.r.squared
+R2#0.002309445
+RsquareAdj(fresh_rda)
+
+#RDAs explained = Proportion explained *R2 *100%
+9.001e-01*0.002309445*100 #RDA1 = 0.2%
+9.991e-02*0.002309445*100 #RDA2 = 0.02
 
 
-#PHRAG Cover, Phrag PRESENCE vs Salinity in RDA Analysis============
-#Define the  StationFront based on  presence of Phragmites:
-veg <- read.csv("CRMS_Marsh_Veg.csv")#From cleaned the CRMS_Marsh_Vegetation.csv to suit R.
-str(veg)#133612 obs. of  24 variables:
-samples2007<-veg[veg$year ==2007,] 
-length(levels(droplevels(samples2007$StationID)))#2558 = number of good plots in 2007 
-#Subset only these 2007 station from entire data set to have a
-#consistent set of plot across years:
-DF2007to2016<- veg[ which (veg$StationID  %in%  samples2007$StationID), c("year", "StationID", "StationFront","StationBack", "SpecCode", "Cover", "Community", "CoverTotal")]
-str(DF2007to2016)#118570 obs. of  8 variables:
-#Reshape to wide format to compute plant composition indices:
-DF2007to2016$Cover <- ifelse(DF2007to2016$Cover ==0,0,1) #turning cover to presence/absence data
-v<-DF2007to2016[,c("StationID","StationFront","Community","SpecCode","Cover","CoverTotal","year")] #select most important variables
-v.wide<-spread(v,key = SpecCode, value = Cover, fill = 0)#species indices can be computed in a wide format only= each species has its own column.
-v.wide$PhragPresence<-ifelse(v.wide$Phraaust == 1, "PhragPresent", "PhragAbsent")
+FreshPlot <- ggplot(data=df.sites, aes(x=RDA1, y=RDA2 ) ) +
+  xlab('RDA1 (0.2 % of variation)') + 
+ylab('RDA2 (0.02 % of variation)') +
+  geom_hline(yintercept=0, colour="black", linetype="dotted" ) +
+  geom_vline(xintercept=0, colour="black", linetype="dotted" ) +
+  geom_segment(data=df.env, aes(x=xOrg, y=yOrg, xend=RDA1, yend=RDA2), size=3,
+               colour="red", arrow=arrow(length=unit(10,"point") ) ) + geom_point()+
+  #geom_text(data=df.env, aes(x=RDA1, y=RDA2, label=var), size=8, colour="darkblue", parse=T) 
+   
+  annotate("text", x = 0.75, y = -0.48, label = c("Phrag Cover"), size=8, color="darkgreen") +
+  annotate("text", x = 0.3, y = 1.07, label = c("Mean Salinity"), size=8, color="darkblue") + theme_bw()
+FreshPlot+ theme(axis.text.x = element_text(size=22,hjust=.5,vjust=.5,face="plain"),
+                axis.text.y = element_text(size=22,hjust=1,vjust=0,face="plain"),  
+                axis.title.x = element_text(size=22,hjust=.5,vjust=0,face="plain"),
+                axis.title.y = element_text(size=22),
+                legend.title = element_text(size=22),
+                plot.title = element_text(size=22, lineheight=1.8, face="bold", hjust = 0.5)) +ggtitle("Lousiana Freshwater Plant Communities")
 
-#StationFront    StationID year PhragCover StationID.Year
-#    CRMS0662 CRMS0662_V50 2010       0.25  CRMS0662_V50.2010
-
-#Mean Salinity:
-env<-read.csv("CRMS_Soil.csv")
-str(env)#194840 obs. of  12 variables // $ StationID: Factor w/ 388 levels
-table(env$year)#sampling effort across years
-#2001  2006  2007  2008  2009  2010  2011  2012  2013  2014  2015  2016  2017 
-#2  3165 12903 20298 24173 24200 22979 23370 19983 13536 11770 15697  2764#Measurements were very haphazard. I am able to look at the change between 2008 & 2014:
-
-#Remove year 2006 and 2017:
-env1<-env[! env$year=="2001" ,] #removing low n years
-env2<-env1[! env1$year=="2006" ,] #removing low n years
-env2007to2016<-env2[! env2$year=="2017" ,] #removing low n years
-table(env2007to2016$year)
-#2007  2008  2009  2010  2011  2012  2013  2014  2015  2016 
-#2903 20298 24173 24200 22979 23370 19983 13536 11770 15697
+#ggsave('FreshPlot.jpeg', graf, dpi=300, height=5, width=8)
+#ggsave('FreshPlot.pdf', graf, dpi=300, height=35, width=35, units='cm')
+#ggsave('FreshPlot.eps', graf, dpi=300, height=34, width=34, units='cm')
 
 
-#CV (coefficient of variation = sd/mean)
-#to measure variability in salinity in each site over time. 
+#Statistics on fit of fresh_rda:
+anova.cca(fresh_rda, step=100)
+#Permutation test for rda under reduced model,Number of permutations: 999
+#Model: rda(formula = df.response1 ~ MeanSalinity + PhragCover, data = freshEnv)
+#            Df  Variance      F Pr(>F)    
+#Model       2 0.0000209 7.2384  0.001 *** ///  Residual 5388 0.0077784 
 
-#Compute mean salinity per StationFrontper year:
-env2007to2016_Salinity<-summarise(group_by(env2007to2016,StationFront,year), MeanSalinity=mean(SoilPorewaterSalinity.ppt),
-                                  SDSalinity=sd(SoilPorewaterSalinity.ppt),CVSalinity= SDSalinity/MeanSalinity)
-env2007to2016_Salinity # data frame [818 x 5]
-#Produce new unique column "StationFront.Year" to be merged by with veg accordingly:
-env2007to2016_Salinity$StationFront.Year<-interaction(env2007to2016_Salinity$StationFront,env2007to2016_Salinity$year)
-env2007to2016_Salinity
-#Merge veg data with salinity data:
-table(v.wide$year)#veg data is alraed cleand = from 2007 to 2016:
-#2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 
-#2558 2559 2558 2558 2558 2558 2558 2558 2558 2557
-
-v.wide$StationFront.Year<-interaction(v.wide$StationFront, v.wide$year)
-v.wide2 <- v.wide[ , - c(2,5)]
-
-PhragPresenceSalinity <- dplyr::left_join(v.wide2,env2007to2016_Salinity, by = "StationFront.Year" )
-names(PhragPresenceSalinity)
-PhragPresenceSalinity$StationID.Year<-interaction(PhragPresenceSalinity$StationID, PhragPresenceSalinity$year)
-
-#Phrag Cover to merge later on to salinity and phrag presence and all:
-PhragCover<- veg[veg$SpecCode=="Phraaust", c("StationID", "year", "Cover")]
-dim(PhragCover)#799 3
-colnames(PhragCover)[3]<- "PhragCover"
-PhragCover$StationID.Year<-interaction(PhragCover$StationID, PhragCover$year)
-head(PhragCover, n=21)
-PhragCover2<-PhragCover[ , - c(1,2)]
-EntireData<-dplyr::left_join( PhragPresenceSalinity, PhragCover2, by = "StationID.Year" )
-EntireData$PhragCover[is.na(EntireData$PhragCover)] <- 0 #tur NAs into zeros
-table(EntireData$PhragCover)
-sum(is.na(EntireData$PhragCover))
-
-#veg and env data to write.csv:======
-dim(EntireData)#25580   416
-CRMSveg<- EntireData[ , 4:407]
-CRMSenv<- EntireData[ , -c(4:407)]#StationID.Year needs splitting:
-CRMSenv2 <- separate(data = CRMSenv, col = StationID.Year, into = c("StationID", "year"), sep = "\\.")
-write.csv(CRMSenv2, file = "EnvDataAll.csv", row.names = FALSE)
-write.csv(CRMSveg, file = "VegDataAll.csv", row.names = FALSE)
-#these are the file for CB to resolve Question 5,
 
