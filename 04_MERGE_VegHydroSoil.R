e0a6327636593b56f9c4f3fd10c8a6c6ecdf3768
pwaryszak <dorpatczyk@yahoo.com>
2018-06-22 15:26:46 -0500

    Watch out for conflicts in functions in tidyverse. Inner_joint replaced with left_joint .Cover values are now consistent with the pivot table values on raw excel file data.

--- 04_MERGE_VegHydroSoil.R
+++ 04_MERGE_VegHydroSoil.R
@@ -1,10 +1,13 @@
 #LOAD Libraries & DATA =====
-library(tidyverse)
+#library(tidyverse)# may produce conflicts as it is set of packages: detach("package:tidyverse", unload=TRUE) 
+#Load single pacakges:
+library(dplyr)
+library(ggplot2)
 library(vegan)
-library(plotrix)
-library(nlme)
-library(chron)
-library(Rmisc)
+#library(plotrix)
+#library(nlme)
+#library(chron)
+#library(Rmisc)
 
 ##### Raw veg data:
 #5 => 75 percent cover; 4 = 50-75 percent cover; 3 = 25-50 percent cover; 2 = 5-25 percent cover; 1 = numerous, but less than 5 percent cover, or scattered, with cover up to 5 percent; + = few, with small cover; and r = rare, solitary, with small cover.
@@ -12,65 +15,59 @@ library(Rmisc)
 veg <- read.csv("CRMS_Marsh_Veg.csv")#Data contains cover values for each plant species. Source:
 levels(veg$Community)#"Brackish" "Freshwater" "Intermediate" "Saline"
 veg$Community<-factor(veg$Community, levels = c( "Freshwater","Intermediate","Brackish","Saline"))
-
-length(unique(veg$StationID))#3665
-length(unique(veg$StationFront))#342
+length(unique(veg$StationID))#3653
+length(unique(veg$StationFront))#343
 
 #Could filter out plots sampled in month 10 or 11 or 12 if I wanted to try harder to make sure
 #that post-hurricanes were not sampled. or I could only look at plots sampled in 10,11,12 
 #if I wanted to make sure only post hurricane is sampled, however there aren't many plots like this
 #Make data wide
 veg2 <- veg %>%
-  select(StationID,StationFront,StationBack,day, month, year,Community,Acer_rubrum:Zizi_Mill)
+  select(StationID,StationFront,StationBack, year,Community,Acer_rubrum:Zizi_Mill)
+dim(veg2)#34961   459
 
-#Make sure each plot has a community for every year (or at least 6 of the 10 years) 2007-2016
+#Make sure each plot has a community for every year
+#(or at least 6 of the 10 years) 2007-2017
 table(veg2$year)#n of records per year.
 
-counts<-veg2%>%
-  group_by(StationID)%>%
+counts<-veg2 %>%
+  group_by(StationID) %>%
   summarise(n())
-counts2<-counts$StationID[which(counts$`n()`>5)]
+
+counts2<-counts$StationID[which(counts$`n()`>5)] 
 veg3<-veg2[which(veg2$StationID%in%counts2),]
-length(unique(veg3$StationID))#3466
+length(unique(veg3$StationID))#3202
 table(veg3$year)
 
 #to make sure there are no duplicates:
 veg3 <- distinct(veg3)
 
-#Using Pawel's method:
-#first define community type by most common type in stationfront
-#then average species comp and div and species rich
-#then merge accretion data with plant community data (veg4)
-
-#Define the community-type in each StationFront based on n of Commuity-type Counts per station:
+#Define community type by most common type in stationfront
 StationComm <- group_by(veg3,StationFront,Community) %>% 
   count(Count=StationFront)
 StationComm#It gives us count of communities per StationFront
+library(tidyr)
 SCwide<- spread(StationComm, key = Community, value = n, fill = 0)#make it wide
 SCwide$WhichMax<-colnames(SCwide)[apply(SCwide,1,which.max)]#while wide we can see which Comm is dominant
-SCwide
-StationCommDefined<-SCwide[, c(1,7)]
+head(SCwide)
+StationCommDefined<-SCwide[, c(1,8)]#NA produced as column 7, not sure why?
 colnames(StationCommDefined)[2] <- "Community" #Renaming WhichMAx back to Community
 StationCommDefined 
 
-
 #Join with veg3 to create a plot/year-level (StationID*year level) data set
-veg4<-veg3%>%
-  rename(Community.yr=Community)%>%
+veg4<-veg3 %>%
+  rename(Community.yr=Community) %>%
   left_join(StationCommDefined,by="StationFront")
-  
-veg4$richness<-specnumber(veg4[,8:456])
-veg4$shannon<-diversity(veg4[,8:456],index="shannon")
-veg4$tot<-rowSums(veg4[,8:456])
+dim(veg4)#33048   460
 
 #rearrange columns
 veg5 <- veg4 %>%
-  select(StationID:Community.yr,Community,richness:tot,Acer_rubrum:Zizi_Mill)
+  select(StationID:Community.yr,Community,Acer_rubrum:Zizi_Mill)
 
 #Plot Phraaust over time========
 m1<-veg5%>%
   group_by(year,Community)%>%
-  summarise(mean=mean(Phra_australis),se=std.error(Phra_australis))
+  summarise(mean=mean(Phra_australis),sd=sd(Phra_australis))
 
 ggplot(m1,aes(x=as.factor(year),y=mean,group=Community, color = Community))+
   labs(x = "Year",y="Mean Cover (%)",colour="Community type")+
@@ -96,48 +93,59 @@ veg6<-veg5%>%
   group_by(StationFront,Community,year)%>%
   summarise_at(vars(Acer_rubrum:Zizi_Mill),mean,na.rm=T)
 
+veg6$StationFront.year<-interaction(veg6$StationFront, veg6$year)#We need that for joining
+ss <- select(veg6, StationFront.year, Spar_patens ) %>%
+  filter(StationFront.year == "CRMS0002.2008")
+ss #   YES! - it should be  66.7% as in pivot table on raw Excel File.
+
+
+
 #then compute richness, shannon, and tot
 #on the new averaged stationfront-level species data (~10 survey plots per StationFront)
 #(rather than have them be the average across the small plots)
 veg6$richness <-specnumber (veg6 [,4:457])
 veg6$shannon  <-diversity  (veg6 [,4:457], index="shannon")
 veg6$tot      <-rowSums    (veg6 [,4:457])
-#write.csv(veg6, file = "CRMS_veg6_11june2018.csv")
-str(veg6) # 3606 obs. of  460 variables:
-
+dim(veg6) #  3464  461
+#write.csv(veg6, file = "CRMS_veg6_22june2018.csv")
 
 #JOIN VEG6 with ENVC4 & ENV5 DATA=====
 #ENVC4 is an object produced in "HydrologicDataECF.R" file off raw CRMS environmental data.
 #WaterData<-as.data.frame(envc4)#Creating data to merge with veg6, or:
 WaterData <- read.csv("CRMS_MeanWaterDepth_envc4.csv")
 dim(WaterData)#data.frame':	3374 obs. of  15 variables:
+names(WaterData)
 #Select Environ data for analysis:
 WaterDataThin <- as.data.frame(select( WaterData, StationFront.year,
                                        meanwaterdepthcm, floodedpercent,
                                        MeanWaterSalinity))
 
-VegData<-as.data.frame(veg6)#veg6 is produced in above lines or saved as csv file:
-#VegData<-read.csv("CRMS_veg6_11june2018.csv")
+#VegData<-as.data.frame(veg6)#veg6 is produced in above lines or saved as csv file:
+VegData <- veg6 # or
+#VegData<-read.csv("CRMS_veg6_22june2018.csv")
 VegData$StationFront.year<-interaction(VegData$StationFront, VegData$year)#We need that for joining
-dim(VegData) #3606  461
+dim(VegData) #3464  461
 VegDataThin <- as.data.frame(select( VegData,  - StationFront))#to ease joining with inner_join:
                              
-#Join Veg Data with WaterData by StationFront.year, some levels of "StationFront.year" do not overlap:
-VegEnvData<-inner_join(VegDataThin, WaterDataThin , by="StationFront.year" )
-dim(VegEnvData) #2849  463
-#write.csv(VegEnvData, file = "CRMS_VegEnvData_11june2018.csv", row.names = F)
+#Join Veg Data with WaterData by StationFront.year,
+VegEnvData<-left_join(VegDataThin, WaterDataThin , by="StationFront.year" )
+dim(VegEnvData)# 3464  464
+#write.csv(VegEnvData, file = "CRMS_VegEnvData_22june2018.csv", row.names = F)
 
 #Join VegEnvData with soil pore water data (env5):
-dim(env5)#2197    8    #USE "env5"  as produced in "SalinityECF.R" file
 env5 <- read.csv("CRMS_Mean_SoilSalinity_env5_11june2018.csv")
-SoilData <- env5 %>%  select ( -year)
+dim(env5)#2198    8    #USE "env5"  as produced in "SalinityECF.R" file
+SoilData <- env5 %>%  select ( -year)#remove year to ease left_join
 
 VegAllEnvData <- left_join(VegEnvData, SoilData  , by="StationFront.year" )
-dim(VegAllEnvData)#2849  469
-#write.csv(VegAllEnvData, file = "VegAllEnvData_11june2018.csv")
+dim(VegAllEnvData)# 3464  470
+checkMe <- select(VegAllEnvData , StationFront.year, Spar_patens) %>%
+  filter(StationFront.year == "CRMS0002.2008")
+checkMe #All good if =  66.7%, we got this value in pivot table in raw csv file.
+#write.csv(VegAllEnvData, file = "VegAllEnvData_22june2018.csv")
 
 #Plot Env Data change over 10 years:======
-VegAllEnvData = read.csv("VegAllEnvData_11june2018.csv")
+VegAllEnvData = read.csv("VegAllEnvData_22june2018.csv")
 VegAllEnvData$Community <- factor(VegAllEnvData$Community, levels = c( "Freshwater","Intermediate","Brackish","Saline"))
 
 #Plot Salinity over years over 4 communities:====
@@ -269,7 +277,6 @@ ggplotRegression(lm(Phraaust ~ Year,
 
 
 #Remove sites that had no Phragmites occurance over the surveyed decade:
-
 Phrag <- select(VegAllEnvData,StationFront.year, Year, Community, Phraaust) %>%
   spread(key = Year, Phraaust, fill = 0) %>%
   mutate (Total_Cover = Year_2007+Year_2008+Year_2009+Year_2010+Year_2011+Year_2012+Year_2013+Year_2014+Year_2015+Year_2016+Year_2017) %>%
