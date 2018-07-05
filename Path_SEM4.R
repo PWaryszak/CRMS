4405249b54b13a6454c1c4cfd315488909ba28dc
pwaryszak <dorpatczyk@yahoo.com>
2018-07-05 11:25:33 -0500

    Path analysis with backward selection towards the best fit model (CRMS data). Covariance NatRich~~Native removed.Extra plots added to scrutinaze the relationships betweent significant terms in semPaths.

--- Path_SEM4.R
+++ Path_SEM4.R
@@ -17,7 +17,7 @@ library("grid")
 NatRich     ~ Depth + Flood  + Soil + Alien  
 Native      ~ Depth + Flood  + Soil + Alien
 Alien       ~ Depth + Flood  + Soil 
-Composition ~ Alien + NatRich + Native
+NatComp ~ Alien + NatRich + Native
 #fit_Apriori_Model <- sem(model1,missing="direct",estimator="ML",data=data)
 
 #"Freshwater" Data (AV = average) ========
@@ -66,11 +66,12 @@ Freshwater.av.env <-  subset( FreshwaterAV, select = c( Mean_SoilSalinity,
 Freshwater.av.env$Introduced_Cover <- Freshwater.av.veg.introduced.total
 Freshwater.av.env$Native_Cover     <- Freshwater.av.veg.native.total
 Freshwater.av.env$Native_Richness  <- Freshwater_Native_Richness 
-#Run PCoA:
-Freshwater_distance <- vegdist(Freshwater_Veg_Matrix , "bray")
+
+#Run PCoA on NatComp (composition of native-only plant species):
+Freshwater_distance <- vegdist(Freshwater.av.veg.native  , "bray")
 Freshwater.pca <- cmdscale(Freshwater_distance , eig = TRUE)
 
-#Combine PCA (Composition) and env data together:
+#Combine PCA (NatComp) and env data together:
 coordinates<-as.data.frame(Freshwater.pca$points[,1:2]) #get MDS1 (x-axis Comp value)
 Freshwater_Data<-cbind(coordinates, Freshwater.av.env)
 
@@ -80,7 +81,7 @@ Freshwater_Data$NatRich     <- scale (Freshwater_Data$Native_Richness)
 Freshwater_Data$Soil        <- scale (Freshwater_Data$Mean_SoilSalinity)
 Freshwater_Data$Alien       <- scale (Freshwater_Data$Introduced_Cover)
 Freshwater_Data$Native      <- scale (Freshwater_Data$Native_Cover)
-Freshwater_Data$Composition <- scale (Freshwater_Data$V1)
+Freshwater_Data$NatComp <- scale (Freshwater_Data$V1)
 Freshwater_Data$Flood       <- scale (Freshwater_Data$floodedpercent)
 Freshwater_Data$Depth       <- scale (Freshwater_Data$meanwaterdepthcm)
 Freshwater_Data$Depth_SD    <- scale (Freshwater_Data$meanwaterdepthcm_SD)
@@ -89,17 +90,17 @@ Freshwater_Data$Depth_SD    <- scale (Freshwater_Data$meanwaterdepthcm_SD)
 #Freshwater SEM:=========
 #All terms significant, backward selection from full model (see "Path_SEM3" R file, line ~200)
 #You can load data directly from previosly saved "Freshwater_Data4SEM.csv"
-Freshwater_Data <- read.csv("Freshwater_Data4SEM.csv")
+#Freshwater_Data <- read.csv("Freshwater_Data4SEM.csv")
 
 model_Freshwater <- '
 #regressions:
 NatRich     ~ Depth + Flood     
 Native      ~ Depth + Flood  + Soil + Alien
-Composition ~ Soil + Alien 
+NatComp ~ Soil + Alien 
 
 #covariances:
-Composition ~~ 0*NatRich
-Native ~~ 0*Composition
+NatComp ~~ 0*NatRich
+Native ~~ 0*NatComp
 '
 fit_Freshwater <- sem(model_Freshwater,missing="direct",estimator="ML",data=Freshwater_Data)
 summary(fit_Freshwater, fit.measures=TRUE, rsquare=T) 
@@ -117,7 +118,12 @@ layFreshwater<-matrix(c(-0.5,  -0.5,
               -0.5,   0.5,
               0,     0.15),  ncol=2,byrow=TRUE)#Alien Position
 
-
+#Check by graphing of some of the significant relationships:
+ggplot(Freshwater_Data,aes(x=Alien,y=NatComp,size=Alien))+
+  geom_point()+
+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Freshwater_Data")
+  
 #"Brackish" Data ========
 VegAllEnvData <- read.csv("VegAllEnvData_03july2018.csv")
 Plant_Info <- read.csv("LA_Plants_Clean.csv")#cleaned on 11 june 2018, has info on what specCode is native/introduced
@@ -169,9 +175,9 @@ BrackishAV.env$Native_Cover     <- BrackishAV.veg.native.total
 BrackishAV.env$Native_Richness  <- Brackish_Native_Richness 
 
 #Run PCoA:
-Brackish_distance <- vegdist(Brackish_Veg_Matrix , "bray")
+Brackish_distance <- vegdist(BrackishAV.veg.native  , "bray")
 Brackish.pca <- cmdscale(Brackish_distance , eig = TRUE)
-#Combine PCA (Composition) and env data together:
+#Combine PCA (NatComp) and env data together:
 coordinates<-as.data.frame(Brackish.pca$points[,1:2]) #get MDS1 (x-axis Comp value)
 Brackish_Data<-cbind(coordinates, BrackishAV.env)
 
@@ -181,10 +187,15 @@ Brackish_Data$NatRich     <- scale (Brackish_Data$Native_Richness)
 Brackish_Data$Soil        <- scale (Brackish_Data$Mean_SoilSalinity)
 Brackish_Data$Alien       <- scale (Brackish_Data$Introduced_Cover)
 Brackish_Data$Native      <- scale (Brackish_Data$Native_Cover)
-Brackish_Data$Composition <- scale (Brackish_Data$V1)
+Brackish_Data$NatComp <- scale (Brackish_Data$V1)
 Brackish_Data$Flood       <- scale (Brackish_Data$floodedpercent)
 Brackish_Data$Depth       <- scale (Brackish_Data$meanwaterdepthcm)
 Brackish_Data$Depth_SD    <- scale (Brackish_Data$meanwaterdepthcm_SD)
+
+#write.csv(Brackish_Data, file = "Brackishr_Data4SEM.csv")
+#All terms significant, backward selection from full model (see "Path_SEM3" R file, line ~200)
+#You can load data directly from previosly saved "Brackish_Data4SEM.csv"
+#Brackish_Data <- read.csv("Brackish_Data4SEM.csv")
 
 #Brackish SEM=========
 #best fit model:
@@ -192,11 +203,11 @@ model_Brackish <- '
 #regressions:
 NatRich     ~  Flood    
 Native      ~  Flood  
-Composition ~  Soil 
+NatComp ~  Soil 
 
 #covariances:
-Composition ~~ 0*NatRich
-Native ~~ 0*Composition
+NatComp ~~ 0*NatRich
+Native ~~ 0*NatComp
 '
 fit_Brackish <- sem(model_Brackish,missing="direct",estimator="ML",data=Brackish_Data)
 summary(fit_Brackish, fit.measures=TRUE, rsquare=T) 
@@ -205,6 +216,13 @@ semPaths(fit_Brackish,"est", intercepts = F, fade = F,
          edge.label.position = 0.25, nCharNodes=6,
          residuals =  F, exoCov = F)
 title("Brackish path analysis (2007-2017, P<0.05)")
+
+#Check by graphing of some of the significant relationships:
+ggplot(Brackish_Data,aes(x=Alien,y=NatComp,size=Alien))+
+  geom_point()+
+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Brackish_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
 
 #"Saline" Data ========
 VegAllEnvData <- read.csv("VegAllEnvData_03july2018.csv")
@@ -257,9 +275,9 @@ Saline.av.env$Native_Cover     <- Saline.av.veg.native.total
 Saline.av.env$Native_Richness  <- Saline_Native_Richness 
 
 #Run PCoA:
-Saline_distance <- vegdist(Saline_Veg_Matrix , "bray")
+Saline_distance <- vegdist(Saline.av.veg.native , "bray")
 Saline.pca <- cmdscale(Saline_distance , eig = TRUE)
-#Combine PCA (Composition) and env data together:
+#Combine PCA (NatComp) and env data together:
 coordinates<-as.data.frame(Saline.pca$points[,1:2]) #get MDS1 (x-axis Comp value)
 Saline_Data<-cbind(coordinates, Saline.av.env)
 
@@ -269,7 +287,7 @@ Saline_Data$NatRich     <- scale (Saline_Data$Native_Richness)
 Saline_Data$Soil        <- scale (Saline_Data$Mean_SoilSalinity)
 Saline_Data$Alien       <- scale (Saline_Data$Introduced_Cover)
 Saline_Data$Native      <- scale (Saline_Data$Native_Cover)
-Saline_Data$Composition <- scale (Saline_Data$V1)
+Saline_Data$NatComp <- scale (Saline_Data$V1)
 Saline_Data$Flood       <- scale (Saline_Data$floodedpercent)
 Saline_Data$Depth       <- scale (Saline_Data$meanwaterdepthcm)
 Saline_Data$Depth_SD    <- scale (Saline_Data$meanwaterdepthcm_SD)
@@ -282,28 +300,37 @@ model_Saline <- '
 NatRich     ~ Depth + Soil 
 Native      ~ Depth + Flood  + Soil 
 Alien       ~ Depth + Flood  + Soil 
-Composition ~  NatRich + Native
+NatComp ~  NatRich + Native
 
 #covariances:
-Composition ~~ 0*NatRich
-Native ~~ 0*Composition
-Alien ~~ 0*Composition
+NatComp ~~ 0*NatRich
+Native ~~ 0*NatComp
+Alien ~~ 0*NatComp
 '
 fit_Saline <- sem(model_Saline,missing="direct",estimator="ML",data=Saline_Data)
-summary(fit_Saline, fit.measures=TRUE, rsquare=T) 
+summary(fit_Saline, fit.measures=TRUE, rsquare=T)
+
+#Design layout of nodes manually for semPaths:
+laySaline<-matrix(c(-0.5,  -0.5,
+                    0.5,  -0.5,
+                    0,     0.10,    #Alien Position
+                    0,    -0.35,
+                    0,     0.5,
+                    0.5,   0.5, 
+                    -0.5,   0.5 ),  ncol=2,byrow=TRUE)
+
 semPaths(fit_Saline,"est", intercepts = F, fade = F, 
          title = T, edge.label.cex = 1.1,sizeMan = 8,
          edge.label.position = 0.25, nCharNodes=6, layout = laySaline,
          residuals =  F, exoCov = F)
 title("Saline path analysis (2007-2017, P<0.05)")
 
-laySaline<-matrix(c(-0.5,  -0.5,
-                        0.5,  -0.5,
-                    0,     0.10,    #Alien Position
-                        0,    -0.35,
-                        0,     0.5,
-                        0.5,   0.5, 
-                        -0.5,   0.5 ),  ncol=2,byrow=TRUE)
+#Check by graphing of some of the significant relationships:
+ggplot(Saline_Data,aes(x=Alien,y=NatComp,size=Alien))+
+  geom_point()+
+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Saline_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
 
 #"Intermediate" Data ========
 VegAllEnvData <- read.csv("VegAllEnvData_03july2018.csv")
@@ -356,9 +383,9 @@ Intermediate.av.env$Native_Cover     <- Intermediate.av.veg.native.total
 Intermediate.av.env$Native_Richness  <- Intermediate_Native_Richness 
 
 #Run PCoA:
-Intermediate_distance <- vegdist(Intermediate_Veg_Matrix , "bray")
+Intermediate_distance <- vegdist(Intermediate.av.veg.native , "bray")
 Intermediate.pca <- cmdscale(Intermediate_distance , eig = TRUE)
-#Combine PCA (Composition) and env data together:
+#Combine PCA (NatComp) and env data together:
 coordinates<-as.data.frame(Intermediate.pca$points[,1:2]) #get MDS1 (x-axis Comp value)
 Intermediate_Data<-cbind(coordinates, Intermediate.av.env)
 
@@ -368,10 +395,15 @@ Intermediate_Data$NatRich     <- scale (Intermediate_Data$Native_Richness)
 Intermediate_Data$Soil        <- scale (Intermediate_Data$Mean_SoilSalinity)
 Intermediate_Data$Alien       <- scale (Intermediate_Data$Introduced_Cover)
 Intermediate_Data$Native      <- scale (Intermediate_Data$Native_Cover)
-Intermediate_Data$Composition <- scale (Intermediate_Data$V1)
+Intermediate_Data$NatComp <- scale (Intermediate_Data$V1)
 Intermediate_Data$Flood       <- scale (Intermediate_Data$floodedpercent)
 Intermediate_Data$Depth       <- scale (Intermediate_Data$meanwaterdepthcm)
 Intermediate_Data$Depth_SD    <- scale (Intermediate_Data$meanwaterdepthcm_SD)
+
+#write.csv(Intermediate_Data, file = "Intermediate_Data4SEM.csv")
+#All terms significant, backward selection from full model (see "Path_SEM3" R file, line ~200)
+#You can load data directly from previosly saved "Intermediate_Data4SEM.csv"
+#Intermediate_Data <- read.csv("Intermediate_Data4SEM.csv")
 
 #Intermediate SEM ========
 #best fit model:
@@ -380,14 +412,22 @@ model_Intermediate <- '
 NatRich     ~ Soil  
 Native      ~  Soil + Alien
 Alien       ~  Soil 
-Composition ~ Alien + NatRich + Native
+NatComp ~ Alien + NatRich + Native
 
 #covariances:
-Composition ~~ 0*NatRich
-Native ~~ 0*Composition
+NatComp ~~ 0*NatRich
+Native ~~ 0*NatComp
 '
 fit_Intermediate <- sem(model_Intermediate,missing="direct",estimator="ML",data=Intermediate_Data)
 summary(fit_Intermediate, fit.measures=TRUE, rsquare=T) 
+
+#Design layout of nodes manually for semPaths:
+layIntermediate<-matrix(c(-0.5,  -0.5,
+                          0.5,  -0.5,
+                          0,     0.10,    #Alien Position
+                          0,    -0.35,
+                          0,     0.5 ),  ncol=2,byrow=TRUE)
+
 
 semPaths(fit_Intermediate,"est", intercepts = F, fade = F, 
          title = T, edge.label.cex = 1.1,sizeMan = 8,
@@ -395,10 +435,54 @@ semPaths(fit_Intermediate,"est", intercepts = F, fade = F,
          residuals =  F, exoCov = F)
 title("Intermediate  path analysis (2007-2017, P<0.05)")
 
-layIntermediate<-matrix(c(-0.5,  -0.5,
-                    0.5,  -0.5,
-                    0,     0.10,    #Alien Position
-                    0,    -0.35,
-                    0,     0.5 ),  ncol=2,byrow=TRUE)
+#Check by graphing of some of the significant relationships:
+ggplot(Intermediate_Data,aes(x=Alien,y=NatComp,size=Alien))+
+  geom_point()+
+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Intermediate_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
 
+#Combine all ggplots:==========
+p1<-ggplot(Freshwater_Data,aes(x=Alien,y=NatComp,size=Alien))+
+  geom_point()+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Freshwater_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
 
+p2<-ggplot(Intermediate_Data,aes(x=Alien,y=NatComp,size=Alien))+
+  geom_point()+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Intermediate_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
+
+p3 <- ggplot(Brackish_Data,aes(x=Alien,y=NatComp,size=Alien))+
+  geom_point()+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Brackish_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
+
+p4 <- ggplot(Saline_Data,aes(x=Alien,y=NatComp,size=Alien))+
+  geom_point()+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Saline_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
+
+grid.arrange(p1,p2,p3,p4, ncol = 2)
+
+pp1<-ggplot(Freshwater_Data,aes(x=Alien,y=Native_Cover,size=Alien))+
+  geom_point()+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Freshwater_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
+
+pp2<-ggplot(Intermediate_Data,aes(x=Alien,y=Native_Cover,size=Alien))+
+  geom_point()+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Intermediate_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
+
+pp3 <- ggplot(Brackish_Data,aes(x=Alien,y=Native_Cover,size=Alien))+
+  geom_point()+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Brackish_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
+
+pp4 <- ggplot(Saline_Data,aes(x=Alien,y=Native_Cover,size=Alien))+
+  geom_point()+  geom_line(stat="smooth",method = "lm",size=.8)+
+  ggtitle ("Saline_Data")+
+  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
+
+grid.arrange(pp1,pp2,pp3,pp4, ncol = 2)
