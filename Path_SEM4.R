38cffdd4970f8bba44b0901162ecb8b0a2a4b582
unknown <BlueCarbon@Bluecarbon-asus.du.deakin.edu.au>
2020-01-14 17:54:08 +1100

    DATA 4 Path analyses.

--- Path_SEM4.R
+++ Path_SEM4.R
@@ -1,4 +1,4 @@
-#Data +libraries + Apriori Model ========
+#Data production for SEM models ========
 VegAllEnvData <- read.csv("VegAllEnvData_03july2018.csv")
 Plant_Info <- read.csv("LA_Plants_Clean.csv")#cleaned on 11 june 2018, has info on what specCode is native/introduced
 dim(VegAllEnvData) # 3498  473
@@ -104,7 +104,7 @@ Freshwater_Data<-cbind(coordinates, Freshwater.av.env)
 #Standarize the variables so their effect size are comparable:
 Freshwater_Data$TotRich     <- scale (Freshwater_Data$richness)
 Freshwater_Data$NatRich     <- scale (Freshwater_Data$Native_Richness)
-Freshwater_Data$Soil        <- scale (Freshwater_Data$Mean_SoilSalinity)
+Freshwater_Data$Soil        <- scale (log(Freshwater_Data$Mean_SoilSalinity))#log to normalize salinity data
 Freshwater_Data$Alien       <- scale (Freshwater_Data$Introduced_Cover)
 Freshwater_Data$NatCov      <- scale (Freshwater_Data$Native_Cover)
 Freshwater_Data$NatComp     <- scale (Freshwater_Data$MDS1)
@@ -112,93 +112,6 @@ Freshwater_Data$Flood       <- scale (Freshwater_Data$floodedpercent)
 Freshwater_Data$Depth       <- scale (Freshwater_Data$meanwaterdepthcm)
 Freshwater_Data$Depth_SD    <- scale (Freshwater_Data$meanwaterdepthcm_SD)
 write.csv(Freshwater_Data, file = "Freshwater_Data4SEM_PCoa.csv", row.names = F)
-
-#Freshwater SEM:=========
-#You can load data directly from previosly saved "Freshwater_Data4SEM.csv"
-Freshwater_Data <- read.csv("Freshwater_Data4SEM.csv")
-Other_Freshwater_Model  <-'
-#regressions:
-NatRich     ~ Depth + Flood  
-Native      ~ Depth + Flood  + Soil + Alien
-NatComp     ~ Soil + Alien 
-
-#covariances:
-NatRich ~~ Native
-NatComp ~~ NatRich
-Native ~~ NatComp
-'
-fit_Other_Freshwater_Model <- sem(Other_Freshwater_Model,missing="direct",
-                         estimator="ML",data=Freshwater_Data)
-summary(fit_Other_Freshwater_Model)
-
-#Best fit model follwoing backward selection on Apriori Model (see line 15):
-model_Freshwater <- '
-#regressions:
-NatRich     ~ Depth + Flood     
-Native      ~ Depth + Flood  + Soil + Alien
-NatComp     ~ Soil + Alien 
-
-#covariances:
-NatComp ~~ 0*NatRich
-Native  ~~ 0*NatComp
-'
-fit_Freshwater <- sem(model_Freshwater,missing="direct",estimator="ML",data=Freshwater_Data)
-summary(fit_Freshwater, fit.measures=TRUE, rsquare=T) 
-
-model_Freshwater3 <- '
-#regressions:
-NatRich     ~ Depth     
-Native      ~ Depth + Soil + Alien
-NatComp     ~ Soil + Alien 
-
-#covariances:
-NatComp ~~ 0*NatRich
-Native  ~~ 0*NatComp
-'
-fit_Freshwater3 <- sem(model_Freshwater3,missing="direct",estimator="ML",data=Freshwater_Data)
-summary(fit_Freshwater3, fit.measures=TRUE, rsquare=T) 
-
-#Compare fit of two Freshwater models:
-AIC(fit_Freshwater, fit_Other_Freshwater_Model,fit_Freshwater3)
-#fit_Freshwater             15 704.5073
-#fit_Other_Freshwater_Model 17 704.4868 
-#fit_Freshwater3            13 672.4939 # Best fit model
-
-#Produce Layout for SemPaths Nodes:
-layFreshwater<-matrix(c(-0.5,  -0.5,
-              0.5,  -0.5,
-              0,   -0.3,
-              -0.5,     0.5,
-              0.5,   0.5, 
-              0,     0.35),  ncol=2,byrow=TRUE)#Alien Position
-
-#Set groups for coloring, if legend = TRUE it will be displayed on the right:
-grps<-list(Climate_Change=c("Depth","Soil","Flood"),
-           Alien_Cover=c("Alien"),
-           Native_Composition = c("NatComp","NatRich","Native"))
-
-#Run SemPaths drawing device:
-semPaths(fit_Freshwater3 ,"est", intercepts = F, fade = F, 
-         title = T, edge.label.cex = 1.3,sizeMan = 12,edge.labels=FALSE,
-         edge.label.position = 0.2, nCharNodes=6,
-         residuals =  F, exoCov = F, 
-         layout = layFreshwater,
-         edge.label.bg = "lightyellow",
-         color=c("lightblue","yellow","lightgreen"),
-         groups=grps, legend = F)
-title("Freshwater path analysis (2007-2017, P < 0.05)", line =2)
-
-#Check by graphing of some of the significant relationships:
-ggplot(Freshwater_Data,aes(x=Alien,y=NatComp))+
-  geom_point()+
-  geom_line(stat="smooth",method = "lm",size=.8)+
-  ggtitle ("Freshwater_Data")
-
-f1<-ggplot(Freshwater_Data,aes(x=Flood,y=NatRich))+
-  geom_point()+
-  geom_line(stat="smooth",method = "lm",size=.8)+
-  ggtitle ("Freshwater_Data")
-
 
 #"Intermediate" Data ========
 VegAllEnvData <- read.csv("VegAllEnvData_03july2018.csv")
@@ -267,7 +180,7 @@ Intermediate_Data<-cbind(coordinates, Intermediate.av.env)
 #Standarize the variables so their effect size are comparable:
 Intermediate_Data$TotRich     <- scale (Intermediate_Data$richness)
 Intermediate_Data$NatRich     <- scale (Intermediate_Data$Native_Richness)
-Intermediate_Data$Soil        <- scale (Intermediate_Data$Mean_SoilSalinity)
+Intermediate_Data$Soil        <- scale (log(Intermediate_Data$Mean_SoilSalinity))#log to normalize salinity data
 Intermediate_Data$Alien       <- scale (Intermediate_Data$Introduced_Cover)
 Intermediate_Data$NatCov      <- scale (Intermediate_Data$Native_Cover)
 Intermediate_Data$NatComp     <- scale (Intermediate_Data$MDS1)
@@ -276,73 +189,7 @@ Intermediate_Data$Depth       <- scale (Intermediate_Data$meanwaterdepthcm)
 Intermediate_Data$Depth_SD    <- scale (Intermediate_Data$meanwaterdepthcm_SD)
 
 write.csv(Intermediate_Data, file = "Intermediate_Data4SEM_PCoA.csv", row.names = F)
-#You can load data directly from previosly saved "Intermediate_Data4SEM.csv"
-Intermediate_Data <- read.csv("Intermediate_Data4SEM.csv")
-
-#Intermediate SEM ========
-#Pick Best fit model following backward selection on Apriori Model (see line 15):
-model_Intermediate <- '
-#regressions:
-NatRich     ~  Soil 
-Native      ~  Soil + Alien
-Alien       ~  Soil 
-NatComp     ~  Soil + NatRich 
-
-#covariances:
-NatComp ~~ 0*NatRich
-Native ~~ 0*NatComp
-'
-fit_Intermediate <- sem(model_Intermediate,missing="direct",estimator="ML",data=Intermediate_Data)
-summary(fit_Intermediate, fit.measures=TRUE, rsquare=T) 
-
-#Model #2:
-model_Intermediate2 <- '
-#regressions:
-NatRich     ~  Soil 
-Native      ~  Soil + Alien
-Alien       ~  Soil 
-NatComp     ~  Soil + NatRich 
-
-#covariances:
-NatComp ~~ NatRich
-Native ~~ NatComp
-Native ~~ NatRich
-'
-fit_Intermediate2 <- sem(model_Intermediate2,missing="direct",estimator="ML",data=Intermediate_Data)
-summary(fit_Intermediate2, fit.measures=TRUE, rsquare=T) 
-
-AIC(fit_Intermediate,  fit_Intermediate2)
-##################df      AIC
-#fit_Intermediate  14 739.8238
-#fit_Intermediate2 17 723.4572 #Best Fit Model
-
-#Design layout of nodes manually for semPaths:
-layIntermediate<-matrix(c(-0.5,  -0.5,
-                          0.5,  -0.5,
-                          0.5,     0.10,    #Alien Position
-                          0,    -0.3,
-                          -0.5,     0.5 ),  ncol=2,byrow=TRUE)
-
-semPaths(fit_Intermediate2,"est", intercepts = F, fade = F, 
-         title = T, edge.label.cex = 1.3,sizeMan = 12,
-         edge.label.position = 0.25, nCharNodes=6, layout = layIntermediate,
-         residuals =  F, exoCov = F,edge.label.bg = "lightyellow",
-         color=c("lightblue","yellow","lightgreen"),
-         groups=grps, legend = F)
-title("Intermediate  path analysis (2007-2017, P<0.05)")
-
-#Check by graphing of some of the significant relationships:
-ggplot(Intermediate_Data,aes(x=Alien,y=NatComp))+
-  geom_point()+
-  geom_line(stat="smooth",method = "lm",size=.8)+
-  ggtitle ("Intermediate_Data")+
-  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
-
-f2<-ggplot(Intermediate_Data,aes(x=Flood,y=NatRich))+
-  geom_point()+
-  geom_line(stat="smooth",method = "lm",size=.8)+
-  ggtitle ("Intermediate_Data")
-
+#You can load data directly from previosly saved "Intermediate_Data4SEM_PCoA.csv"
 
 #"Brackish" Data ========
 VegAllEnvData <- read.csv("VegAllEnvData_03july2018.csv")
@@ -413,7 +260,7 @@ Brackish_Data<-cbind(coordinates,BrackishAV.env)
 #Standarize the variables so their effect size are comparable:
 Brackish_Data$TotRich     <- scale (Brackish_Data$richness)
 Brackish_Data$NatRich     <- scale (Brackish_Data$Native_Richness)
-Brackish_Data$Soil        <- scale (Brackish_Data$Mean_SoilSalinity)
+Brackish_Data$Soil        <- scale (log(Brackish_Data$Mean_SoilSalinity))#log to normalize salinity data
 Brackish_Data$Alien       <- scale (Brackish_Data$Introduced_Cover)
 Brackish_Data$NatCov      <- scale (Brackish_Data$Native_Cover)
 Brackish_Data$NatComp     <- scale (Brackish_Data$MDS1)
@@ -423,89 +270,7 @@ Brackish_Data$Depth_SD    <- scale (Brackish_Data$meanwaterdepthcm_SD)
 
 write.csv(Brackish_Data, file = "Brackish_Data4SEM_PCoA.csv", row.names = F)
 #All terms significant, backward selection from full model (see "Path_SEM3" R file, line ~200)
-#You can load data directly from previosly saved "Brackish_Data4SEM.csv"
-#Brackish_Data <- read.csv("Brackish_Data4SEM.csv")
-
-#Brackish SEM=========
-#Best fit model follwoing backward selection on Apriori Model (see line 15):
-#1:
-model_Brackish <- '
-#regressions:
-NatRich     ~  Flood  + Soil
-Native      ~  Flood 
-NatComp     ~  Soil
-
-#covariances:
-NatComp ~~ 0*NatRich
-Native ~~ 0*NatComp
-NatRich ~~ 0*Native
-'
-fit_Brackish <- sem(model_Brackish,missing="direct",estimator="ML",data=Brackish_Data)
-summary(fit_Brackish, fit.measures=TRUE, rsquare=T) 
-
-#2:
-model_Brackish2 <- '
-#regressions:
-NatRich     ~  Flood  + Soil   
-Native      ~  Flood 
-NatComp     ~  Soil
-
-#covariances:
-NatComp ~~ NatRich
-Native ~~ NatComp
-Native ~~ NatRich
-'
-fit_Brackish2 <- sem(model_Brackish2,missing="direct",estimator="ML",data=Brackish_Data)
-summary(fit_Brackish2, fit.measures=TRUE, rsquare=T) 
-
-#3
-model_Brackish3 <- '
-#regressions:
-NatRich     ~  Depth  + Soil
-Native      ~  Depth 
-NatComp     ~  Soil
-
-#covariances:
-NatComp ~~ 0*NatRich
-Native ~~ 0*NatComp
-NatRich ~~ 0*Native
-'
-fit_Brackish3 <- sem(model_Brackish3,missing="direct",estimator="ML",data=Brackish_Data)
-summary(fit_Brackish3, fit.measures=TRUE, rsquare=T) 
-
-AIC(fit_Brackish,fit_Brackish2,fit_Brackish3)
-##############df      AIC
-#fit_Brackish  10 557.2846 
-#fit_Brackish2 13 557.5115 
-#fit_Brackish3 10 554.2543 # marginally better!
-
-#Produce Layout for SemPaths Nodes:
-layBrackish<-matrix(c(-0.5,  -0.5,
-                        0.5,  -0.5,
-                        0,    -0.3,
-                        -0.5,     0.5,
-                        0.5,   0.5),  ncol=2,byrow=TRUE)#Alien Position
-#Plot:
-semPaths(fit_Brackish3,"est", intercepts = F, fade = F, 
-         title = T, edge.label.cex = 1.3,sizeMan = 12,
-         edge.label.position = 0.25, nCharNodes=6,
-         layout = layBrackish,
-         residuals =  F, exoCov = F,edge.label.bg = "lightyellow",
-         color=c("lightblue","yellow","lightgreen"),
-         groups=grps, legend = F)
-title("Brackish path analysis (2007-2017, P<0.05)")
-
-#Check by graphing of some of the significant relationships:
-ggplot(Brackish_Data,aes(x=Alien,y=NatComp))+
-  geom_point()+
-  geom_line(stat="smooth",method = "lm",size=.8)+
-  ggtitle ("Brackish_Data")+
-  labs(caption = "(based on data from: VegAllEnvData_03july2018.csv)")
-
-f3<-ggplot(Brackish_Data,aes(x=Flood,y=NatRich))+
-  geom_point()+
-  geom_line(stat="smooth",method = "lm",size=.8)+
-  ggtitle ("Brackish_Data")
+#You can load data directly from previosly saved "Brackish_Data4SEM_PCoA.csv"
 
 #"Saline" Data ========
 VegAllEnvData <- read.csv("VegAllEnvData_03july2018.csv")
@@ -558,105 +323,32 @@ Saline.av.env$Native_Cover     <- Saline.av.veg.native.total
 Saline.av.env$Native_Richness  <- Saline_Native_Richness 
 
 #Run PCoA:
-Saline_distance <- vegdist(Saline.av.veg.native , "bray")
-Saline.pca <- cmdscale(Saline_distance , eig = TRUE)
-#Combine PCA (NatComp) and env data together:
-coordinates<-as.data.frame(Saline.pca$points[,1:2]) #get MDS1 (x-axis Comp value)
+#Remove columns with only one record:
+Saline.av.veg.native1<- Saline.av.veg.native [  , colSums(Saline.av.veg.native > 0) > 1]#Remove plots/species with less than 1 record in them
+dim(Saline.av.veg.native) ; dim(Saline.av.veg.native1)
+#[1] 64 33
+#[1] 64 23
+
+#Run PCoA:
+mc_Saline<-capscale(Saline.av.veg.native1 ~ 1, distance="bray")#This produces a PCoA, (principle coordinates analysis, otherwise known as metric multidimensional scaling).
+#PCA is not great to use with species comp data b/c it uses euclidean distance which essentially means that if two plots both have 0 for a species it calls them similar. Bray dissimilarity fixes this and only uses species that are present in the plots to calculate similarity/dissimilarity. 
+coordinates<-as.data.frame(scores(mc_Saline)$sites) #get MDS1 and 2
+#Combine PCoA (NatComp) and env data together:
 Saline_Data<-cbind(coordinates, Saline.av.env)
 
 #Standarize the variables so their effect size are comparable:
 Saline_Data$TotRich     <- scale (Saline_Data$richness)
 Saline_Data$NatRich     <- scale (Saline_Data$Native_Richness)
-Saline_Data$Soil        <- scale (Saline_Data$Mean_SoilSalinity)
+Saline_Data$Soil        <- scale (log(Saline_Data$Mean_SoilSalinity))#log to normalize salinity data
 Saline_Data$Alien       <- scale (Saline_Data$Introduced_Cover)
-Saline_Data$Native      <- scale (Saline_Data$Native_Cover)
-Saline_Data$NatComp <- scale (Saline_Data$V1)
+Saline_Data$NatCov      <- scale (Saline_Data$Native_Cover)
+Saline_Data$NatComp <- scale (Saline_Data$MDS1)
 Saline_Data$Flood       <- scale (Saline_Data$floodedpercent)
 Saline_Data$Depth       <- scale (Saline_Data$meanwaterdepthcm)
 Saline_Data$Depth_SD    <- scale (Saline_Data$meanwaterdepthcm_SD)
 
-#write.csv(Saline_Data, file = "Saline_Data4SEM.csv")
-#You can load data directly from previosly saved "Saline_Data4SEM.csv"
-Saline_Data <- read.csv("Saline_Data4SEM.csv")
-
-#Saline SEM===========
-#Compare models to find best fit: (Best fit model follwoing backward selection on Apriori Model (see line 15))
-#1:
-Other_Model <-'
-#regressions:
-NatRich     ~    Soil
-Native      ~  Flood  + Soil
-NatComp     ~  NatRich 
-
-#Coviariances:
-Native ~~ NatComp
-Native ~~ NatRich
-
-'
-fit_Other_Model <- sem(Other_Model,missing="direct",estimator="ML",
-                         data=Saline_Data)
-summary(fit_Other_Model)
-semPaths(fit_Other_Model,"est", intercepts = F, fade = F, 
-         title = T, edge.label.cex = 1.1,sizeMan = 8,
-         edge.label.position = 0.25, nCharNodes=6,
-         residuals =  F, exoCov = F)
-
-#2:
-model_Saline <- '
-#regressions:
-#regressions:
-NatRich     ~ Depth + Soil 
-Native      ~ Depth + Soil 
-Alien       ~ Depth + Soil 
-NatComp     ~  NatRich + Native
-
-#covariances:
-NatComp ~~ 0*NatRich
-Native ~~ 0*NatComp
-'
-fit_Saline <- sem(model_Saline,missing="direct",estimator="ML",data=Saline_Data)
-summary(fit_Saline, fit.measures=TRUE, rsquare=T)
-
-semPaths(fit_Saline,"est", intercepts = F, fade = F, 
-         title = T, edge.label.cex = 1.1,sizeMan = 8,
-         edge.label.position = 0.25, nCharNodes=6,
-         residuals =  F, exoCov = F)
-
-
-#3:
-model_Saline3 <- '
-#regressions
-NatRich     ~  Soil
-Native      ~ Depth  + Soil
-NatComp     ~ Depth  + Soil
-
-#covariances:
-NatComp ~~ NatRich
-Native ~~ NatComp
-Native ~~ NatRich
-'
-
-AIC(fit_Saline, fit_Other_Model,fit_Saline3 )#the Other_Model seems working well:
-###              df      AIC
-#fit_Saline      17 971.9086 #Best Fit = Printed.
-#fit_Other_Model 12 794.5944
-#fit_Saline3     14 790.9772
-
-#Design layout of nodes manually for semPaths:
-laySaline<-matrix(c(-0.5,  -0.5,
-                    0.5,  -0.5,
-                    0,     -0.3,    #NatComp Position
-                    -0.5,    0.5,
-                    0.5,     0.5),  ncol=2,byrow=TRUE)
-
-semPaths(fit_Saline3,"est", intercepts = F, fade = F, 
-         title = T, edge.label.cex = 1.3,sizeMan = 12,
-         edge.label.position = 0.2, nCharNodes=6, layout = laySaline,
-         residuals =  F, exoCov = F,edge.label.bg = "lightyellow",
-         color=c("lightblue","yellow","lightgreen"),
-         groups=grps, legend = F)
-title("Saline path analysis (2007-2017, P<0.05)")
-
+write.csv(Saline_Data, file = "Saline_Data4SEM_PCoA.csv", row.names = F)
+#You can load data directly from previosly saved "Saline_Data4SEM_PCoA.csv"
 
 #Checking relationships in Data========
 #Check by graphing of some of the significant relationships:
