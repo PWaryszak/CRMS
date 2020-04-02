0d7d30868c5cb4b78df582a836c1a626e2b2e8c6
unknown <BlueCarbon@Bluecarbon-asus.du.deakin.edu.au>
2020-04-02 14:55:30 +1100

    Plot introduced + ANOVA across communities.

--- Invasive.R
+++ Invasive.R
@@ -15,13 +15,13 @@ Plant_Info_Invasive <- Plant_Info  %>% filter (Plant_Info$nat == "introduced") %
                                       select(specCode)
 
 inv_names<-as.character(droplevels((Plant_Info_Invasive$specCode)))
-MyCols <- rbind(inv_names, "Community")#To subset later on:
+MyCols <- rbind(inv_names, "Community","StationFront")#To subset later on:
 
 inv_data <- VegAllEnvData [ , colnames(VegAllEnvData) %in% MyCols] #select introduced species columns
-dim(inv_data)
+dim(inv_data)#3498   45
 inv_data $ richness <- specnumber(inv_data[, 2:44])
 
-#PLOT Richness ===========
+#PLOT Richness:========
 RichnessSum <- summarySE(inv_data, measurevar = "richness",
                          groupvars = "Community", na.rm = T)
 RichnessSum$Community<-factor(RichnessSum$Community,
@@ -62,5 +62,65 @@ ggplot(CoverSum, aes(x=Community, y= Cover, fill = Community)) +
         axis.text.y  = element_text(size = 14),
         plot.title = element_text(hjust= 0.5, size =16))
 
-
 #ggsave(dpi=600, width = 7, height = 5, filename = "Rplot_CRMS_Inv_Cover.pdf")
+
+#AOV on Introduced_Cover======
+inv_data $Introduced_Cover<- rowSums(inv_data[, 2:44])
+
+data_aov <- 
+  group_by(inv_data ,StationFront,Community) %>% #Compute means of all variables per StationFront (site), over all years:
+  na.omit() %>%
+  summarise_at(vars(Introduced_Cover,
+                    Alte_caracasana:Vign_angularis), mean,na.rm=T)
+
+( Cover.aov <- aov(Introduced_Cover ~ Community, data_aov ) )
+summary(Cover.aov)
+TukeyHSD(Cover.aov)
+
+#OUTPUT:
+#Call:  aov(formula = Introduced_Cover ~ Community, data = data_aov)
+
+Terms:          Community Residuals
+Sum of Squares   5206.192 14831.110
+Deg. of Freedom         3       211
+
+Residual standard error: 8.38389
+Estimated effects may be unbalanced
+
+> summary(Cover.aov)
+Df Sum Sq Mean Sq F value   Pr(>F)    
+Community     3   5206  1735.4   24.69 9.85e-14 ***
+Residuals   211  14831    70.3                     
+
+> TukeyHSD(Cover.aov)
+Tukey multiple comparisons of means
+95% family-wise confidence level
+
+Fit: aov(formula = Introduced_Cover ~ Community, data = data_aov)
+
+$Community
+diff                                     lwr       upr     p adj
+Freshwater-Brackish      12.041501   7.424453 16.658549 0.0000000
+Intermediate-Brackish     5.634021   1.249470 10.018572 0.0056613
+Saline-Brackish          -0.451822  -4.781806  3.878162 0.9930821
+Intermediate-Freshwater  -6.407480 -10.596473 -2.218487 0.0005873
+Saline-Freshwater       -12.493323 -16.625166 -8.361480 0.0000000
+Saline-Intermediate      -6.085843  -9.956152 -2.215534 0.0003815
+
+
+#Most common species========
+most <- VegAllEnvData %>%
+  na.omit() %>%
+  group_by(Community) %>% #Compute means of all variables per StationFront (site), over all years:
+  summarise_at(vars(Acer_rubrum:Ziza_miliacea), sum ,na.rm=T) %>%
+  gather(specCode,cover, - Community)%>%
+  arrange(desc(cover,Community))
+
+most_nat <- left_join(most, Plant_Info, by = "specCode")
+
+write.csv(most_nat, file = "TopSpecies_SUM_Nat_Inv_CRMS.csv", row.names = F)
+
+#Subset native/introduced Veg matrix where colSums are > 0:
+Freshwater_Veg_Matrix <- as.data.frame (subset ( FreshwaterAV, select = c(Acer_rubrum:Ziza_miliacea)))
+Freshwater_Veg_Sums <- colSums(Freshwater_Veg_Matrix)
+which.max(Freshwater_Veg_Sums)#Pani_hemitomon
